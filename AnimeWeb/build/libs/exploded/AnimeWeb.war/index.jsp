866c235b9cef5ce4bcf7df1bf14814dfecc7ec53
Taki123s <20130305@st.hcmuaf.edu.vn>
2023-12-14 11:16:08 +0700

    prepare merg in to main

--- AnimeWeb/build/libs/exploded/AnimeWeb.war/index.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
-<c:redirect url ="/anime-main/Index"/>
--- AnimeWeb/src/main/java/security/DSA.java
+++ AnimeWeb/src/main/java/security/DSA.java
@@ -2,6 +2,7 @@ package security;
 
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 public class DSA {
@@ -13,7 +14,6 @@ public class DSA {
         KeyPair keyPair = keyGen.generateKeyPair();
         publicKey = keyPair.getPublic();
         privateKey = keyPair.getPrivate();
-
     }
 
     public byte[] signMessage(String data, PrivateKey privateKey) throws Exception {
@@ -60,8 +60,15 @@ public static boolean verifyPublicKey(String keyInput) throws NoSuchAlgorithmExc
        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
        keyFactory.generatePublic(keySpec);
        return true;
-
 }
+    public static PrivateKey verifyPrivateKey(String keyInput) throws Exception {
+        if (keyInput.trim().equals(""))
+            return null;
+        byte[] privateKeyBytes = Base64.getDecoder().decode(keyInput);
+        KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        return keyFactory.generatePrivate(keySpec);
+    }
     public PublicKey getPublicKey() {
         return publicKey;
     }
@@ -80,14 +87,13 @@ public static boolean verifyPublicKey(String keyInput) throws NoSuchAlgorithmExc
 
     public static void main(String[] args) throws Exception {
 
+        DSA dsa = new DSA();
+        dsa.generateKey();
+        System.out.println(dsa.privateKeyToBase64());
+//        System.out.println(dsa.publicKeyToBase64());
+//        verifyPublicKey("MIIDQjCCAjUGByqGSM44BAEwggIoAoIBAQCPeTXZuarpv6vtiHrPSVG28y7FnjuvNxjo6sSWHz79NgbnQ1GpxBgzObgJ58KuHFObp0dbhdARrbi0eYd1SYRpXKwOjxSzNggooi/6JxEKPWKpk0U0CaD+aWxGWPhL3SCBnDcJoBBXsZWtzQAjPbpUhLYpH51kjviDRIZ3l5zsBLQ0pqwudemYXeI9sCkvwRGMn/qdgYHnM423krcw17njSVkvaAmYchU5Feo9a4tGU8YzRY+AOzKkwuDycpAlbk4/ijsIOKHEUOThjBopo33fXqFD3ktm/wSQPtXPFiPhWNSHxgjpfyEc2B3KI8tuOAdl+CLjQr5ITAV2OTlgHNZnAh0AuvaWpoV499/e5/pnyXfHhe8ysjO65YDAvNVpXQKCAQAWplxYIEhQcE51AqOXVwQNNNo6NHjBVNTkpcAtJC7gT5bmHkvQkEq9rI837rHgnzGC0jyQQ8tkL4gAQWDt+coJsyB2p5wypifyRz6Rh5uixOdEvSCBVEy1W4AsNo0fqD7UielOD6BojjJCilx4xHjGjQUntxyaOrsLC+EsRGiWOefTznTbEBplqiuH9kxoJts+xy9LVZmDS7TtsC98kOmkltOlXVNb6/xF1PYZ9j897buHOSXC8iTgdzEpbaiH7B5HSPh++1/et1SEMWsiMt7lU92vAhErDR8C2jCXMiT+J67ai51LKSLZuovjntnhA6Y8UoELxoi34u1DFuHvF9veA4IBBQACggEAYUvlSJoYZwJv31y7pU+oDcZm5QVtiySGXTclcokv30wP1r/ED6lX814d0hy74Xw2gAloFeAltCynRfsUqkE5ZpTwku8uNERv3xCT/j3MEJKaOpOVqFCW2GLbXxG1XKuTfXO09TvCfXX+iiaF70F7NOMC922RBQ/3DhM3luzgulCI0g/Y3EZPfjAagV+eJH+QD4cLrHE5ENTwtGvl8oYl0YVYHK1wRgtDfeJAJK3qJG7dTv2lO4gKodULRNzYRAvL7pFxX9xfrzGj79YpW5QFfyzVFtbZiAoIAAMrO/b6AHwC0EYIDTIyPHdv0vMr5vfeggcdnjzZ9hROwnhxwrv8SA==");
 //
-//        KeyPair keyPair = generateKeyPair();
-//        PrivateKey privateKey = keyPair.getPrivate();
-//        PublicKey publicKey = keyPair.getPublic();
-//        System.out.println(toBase64(publicKey.getEncoded()));
-
-
-//
+        verifyPrivateKey("MIICXAIBADCCAjUGByqGSM44BAEwggIoAoIBAQCPeTXZuarpv6vtiHrPSVG28y7FnjuvNxjo6sSWHz79NgbnQ1GpxBgzObgJ58KuHFObp0dbhdARrbi0eYd1SYRpXKwOjxSzNggooi/6JxEKPWKpk0U0CaD+aWxGWPhL3SCBnDcJoBBXsZWtzQAjPbpUhLYpH51kjviDRIZ3l5zsBLQ0pqwudemYXeI9sCkvwRGMn/qdgYHnM423krcw17njSVkvaAmYchU5Feo9a4tGU8YzRY+AOzKkwuDycpAlbk4/ijsIOKHEUOThjBopo33fXqFD3ktm/wSQPtXPFiPhWNSHxgjpfyEc2B3KI8tuOAdl+CLjQr5ITAV2OTlgHNZnAh0AuvaWpoV499/e5/pnyXfHhe8ysjO65YDAvNVpXQKCAQAWplxYIEhQcE51AqOXVwQNNNo6NHjBVNTkpcAtJC7gT5bmHkvQkEq9rI837rHgnzGC0jyQQ8tkL4gAQWDt+coJsyB2p5wypifyRz6Rh5uixOdEvSCBVEy1W4AsNo0fqD7UielOD6BojjJCilx4xHjGjQUntxyaOrsLC+EsRGiWOefTznTbEBplqiuH9kxoJts+xy9LVZmDS7TtsC98kOmkltOlXVNb6/xF1PYZ9j897buHOSXC8iTgdzEpbaiH7B5HSPh++1/et1SEMWsiMt7lU92vAhErDR8C2jCXMiT+J67ai51LKSLZuovjntnhA6Y8UoELxoi34u1DFuHvF9veBB4CHH084AJ38jI6lkNWrcIFGJWtMW42+z4MNLkvOng=");
 //
 //        String message = "This is a test message";
 //        byte[] signatureBytes = signMessage(message, privateKey);
